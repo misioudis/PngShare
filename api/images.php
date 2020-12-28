e319c42d86a05d55c9b31ba954f0c1d900ea73f7
Trojanzaro <anastasiszaro@gmail.com>
2020-12-29 00:24:04 +0200

    Added mechanism for uploading files in temporary locatin

---
+++ api/images.php
@@ -0,0 +1,57 @@
+<?php
+    include 'config.php';
+
+    //Get username
+    $user = json_decode(
+        base64_decode(
+            explode('.', explode(
+                    'Bearer', getallheaders()["Authorization"]
+                    )[1]
+                )[1]
+            , true)
+    , true)["un"];
+
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        include 'auth.php';
+
+        // file name
+        $filename = $_FILES['file']['name'];
+
+        // Location
+        $canonical_filename = time(). '_' .$filename;
+        $location = '/png_share_data/'.$user. '/tmp/' .$canonical_filename;
+
+        // file extension
+        $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+        $file_extension = strtolower($file_extension);
+
+        // Valid image extensions
+        $image_ext = array("jpg","png","jpeg","gif");
+
+        $response = 0;
+        if(in_array($file_extension,$image_ext)){
+            // Upload file
+            if(move_uploaded_file($_FILES['file']['tmp_name'], $location)){
+                $response = $location;
+            }
+        }
+
+        echo json_encode(array("code" => 200, "path" => $canonical_filename));
+
+    } elseif ($_SERVER['REQUEST_METHOD'] == "GET"){
+        $db_o = new DB_O();
+        $db_o = $db_o->get_db();
+        $stmt = $db_o->prepare("SELECT username FROM users WHERE id = ?;");
+        $stmt->bind_param("s", $_GET["uid"]);
+        $stmt->execute();
+        $stmt->bind_result($un);
+        $stmt->fetch();
+        if($_GET["temp"] == "false")
+            readfile('/png_share_data/'.$un.'/'.$_GET["path"]);
+        else
+        readfile('/png_share_data/'.$un.'/tmp/'.$_GET["path"]);
+    } elseif ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+
+    }
+?>
--- js/profile_functions.js
+++ js/profile_functions.js
@@ -16,3 +16,31 @@ function userName() {
     else 
         document.getElementById('username').innerHTML = JSON.parse(atob(window.localStorage.getItem('token').split('.')[1]))["un"];
 }
+
+function getToken() {
+    if (window.localStorage.getItem("token") === null)
+        return window.sessionStorage.getItem('token');
+    else
+        return window.localStorage.getItem('token');
+  }
+
+  function getUserID() {
+    return (JSON.parse(atob(getToken().split('.')[1]))).uid;
+  }
+  
+  function selectImage() {
+    let div = document.getElementById("imagePreview");
+    let file = document.getElementById("imageToUpload").files[0];
+    let data = new FormData();
+    data.append('file', file);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        div.innerHTML = '<img src="api/images.php?temp=true&uid='+getUserID()+'&path='+JSON.parse(this.responseText).path + '" style="max-height: 100%; max-width: 100%;"></img>'
+      }
+    };
+    xhttp.open("POST", "api/images.php", true);
+    xhttp.setRequestHeader('Authorization', 'Bearer ' + getToken());
+    xhttp.send(data);
+  }
