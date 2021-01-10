9a971c763449962679f599915467aca9426de5d6
Trojanzaro <anastasiszaro@gmail.com>
2021-01-10 17:16:43 +0200

    Added progress bar for uploading pictures

--- js/profile_functions.js
+++ js/profile_functions.js
@@ -69,6 +69,13 @@ function selectImage() {
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
+        if(this.readyState == 3) {
+            div.innerHTML = '<div class="progress">' +
+            '<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:100%">'+
+              'Uploading'+
+            '</div>'+
+          '</div>';
+        }
         if (this.readyState == 4 && this.status == 200) {
 
             //in case there are already files in temp
